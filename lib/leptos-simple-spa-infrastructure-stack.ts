ef7719729c08d5cdb2f4d7e6b1ec0868a69bdebe
Tegh Aiden <taiden1026@gmail.com>
2024-08-25 17:11:32 -0400

    Enable public access

--- lib/leptos-simple-spa-infrastructure-stack.ts
+++ lib/leptos-simple-spa-infrastructure-stack.ts
@@ -1,16 +1,42 @@
-import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class LeptosSimpleSpaInfrastructureStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+import { CfnBucket, CfnBucketPolicy } from 'aws-cdk-lib/aws-s3'
+
+export class LeptosSimpleSpaInfrastructureStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucketName: string = "leptos-simple-spa-bucket";
+    
+    const siteBucket = new CfnBucket(this, "LeptosSimpleSPABucket", {
+      bucketName: bucketName,
+      publicAccessBlockConfiguration: {
+        blockPublicPolicy: false,
+      },
+      websiteConfiguration: {
+        indexDocument: 'index.html',
+      }
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LeptosSimpleSpaInfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+  const publicAccessBucketPolicy = new CfnBucketPolicy(this, "PublicAccessBucketPolicy", {
+      bucket: bucketName,
+      policyDocument: {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "PublicReadGetObject",
+                "Effect": "Allow",
+                "Principal": "*",
+                "Action": [
+                    "s3:GetObject"
+                ],
+                "Resource": [
+                    `arn:aws:s3:::${bucketName}/*`
+                ]
+            }
+        ]
+    }
+  });
+}
 }
