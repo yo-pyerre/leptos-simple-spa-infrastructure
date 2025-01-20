62008c380d90b5a3a2f4b2a43fadbd4cdd72f633
Tegh Aiden <taiden1026@gmail.com>
2025-01-20 18:25:37 -0500

    Added prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,12 +4,34 @@ import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  {ignores: [
+  {
+    files: [
+      "**/*.{js,mjs,cjs,ts}"
+    ]
+  },
+  {
+    languageOptions: { 
+      globals: globals.browser 
+    }
+  },
+  {
+    ignores: [
     'node_modules/',
     'cdk.out/',
     'dist/'
-  ]},
+    ]
+  },
+  {
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:prettier/recommended"
+    ]
+  },
+  {
+    rules: {
+      "prettier/prettier": "error"
+    }
+  },
   ...tseslint.configs.recommended,
 ]; 
